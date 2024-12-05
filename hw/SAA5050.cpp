30fd1724632c0ba13cffe1a8bf4d00c18aea8a6a
Nico Vijlbrief <nico.vijlbrief@outlook.com>
2024-12-05 22:06:57 +0100

    Better code, fits window, proportions, 40x25 and scalable. but not how the SAA5050 works hehe

--- hw/SAA5050.cpp
+++ hw/SAA5050.cpp
@@ -19,19 +19,22 @@ SAA5050::~SAA5050() {
 void SAA5050::checkAddressSpace(uint16_t address, uint8_t* memory) {
     // Get the window size
     int windowWidth, windowHeight;
-    SDL_GetRendererOutputSize(renderer, &windowWidth, &windowHeight);
+    if (SDL_GetRendererOutputSize(renderer, &windowWidth, &windowHeight) != 0) {
+        std::cerr << "Failed to get renderer output size! SDL_Error: " << SDL_GetError() << std::endl;
+        return;
+    }
 
     // Calculate the scale factor
-    int scaleX = windowWidth / 640; // Assuming 80 characters per line and each character is 8 pixels wide
-    int scaleY = windowHeight / 192; // Assuming 24 lines and each character is 8 pixels high
+    int scaleX = windowWidth / (40 * 6); // 40 characters per line, each character is 5 pixels wide + 1 pixel space
+    int scaleY = windowHeight / (24 * 10); // 24 lines, each character is 9 pixels high + 1 pixel space
     int scale = std::min(scaleX, scaleY);
 
     if (address >= startAddress && address <= endAddress) {
         // Call render function or handle memory changes
-        for (int j = 0; j < 24; ++j) { // Assuming 24 lines of characters
-            for (int i = 0; i < 80; ++i) { // Assuming SAA5050 can draw 80 characters per line
-                uint8_t value = memory[address + i + j * 80];
-                renderCharacter(value, i * 8 * scale, j * 8 * scale, scale); // Assuming each character is 8 pixels wide and 8 pixels high
+        for (int j = 0; j < 24; ++j) { // 24 lines of characters
+            for (int i = 0; i < 40; ++i) { // 40 characters per line
+                uint8_t value = memory[address + i + j * 40];
+                renderCharacter(value, i * 6 * scale, j * 10 * scale, scale); // Each character is 5 pixels wide + 1 pixel space, 9 pixels high + 1 pixel space
             }
         }
     }
@@ -39,36 +42,36 @@ void SAA5050::checkAddressSpace(uint16_t address, uint8_t* memory) {
 }
 
 void SAA5050::renderCharacter(uint8_t value, int x, int y, int scale) {
-    // Define a simple 8x8 font for characters 0-9 and A-F
-    uint8_t font[16][8] = {
-        {0b00111100, 0b01100110, 0b01101110, 0b01110110, 0b01100110, 0b01100110, 0b00111100, 0b00000000}, // 0
-        {0b00011000, 0b00111000, 0b00011000, 0b00011000, 0b00011000, 0b00011000, 0b01111110, 0b00000000}, // 1
-        {0b00111100, 0b01100110, 0b00000110, 0b00001100, 0b00110000, 0b01100000, 0b01111110, 0b00000000}, // 2
-        {0b00111100, 0b01100110, 0b00000110, 0b00011100, 0b00000110, 0b01100110, 0b00111100, 0b00000000}, // 3
-        {0b00001100, 0b00011100, 0b00101100, 0b01001100, 0b01111110, 0b00001100, 0b00001100, 0b00000000}, // 4
-        {0b01111110, 0b01100000, 0b01111100, 0b00000110, 0b00000110, 0b01100110, 0b00111100, 0b00000000}, // 5
-        {0b00111100, 0b01100110, 0b01100000, 0b01111100, 0b01100110, 0b01100110, 0b00111100, 0b00000000}, // 6
-        {0b01111110, 0b01100110, 0b00001100, 0b00011000, 0b00110000, 0b00110000, 0b00110000, 0b00000000}, // 7
-        {0b00111100, 0b01100110, 0b01100110, 0b00111100, 0b01100110, 0b01100110, 0b00111100, 0b00000000}, // 8
-        {0b00111100, 0b01100110, 0b01100110, 0b00111110, 0b00000110, 0b01100110, 0b00111100, 0b00000000}, // 9
-        {0b00111100, 0b01100110, 0b01100110, 0b01111110, 0b01100110, 0b01100110, 0b01100110, 0b00000000}, // A
-        {0b01111100, 0b01100110, 0b01100110, 0b01111100, 0b01100110, 0b01100110, 0b01111100, 0b00000000}, // B
-        {0b00111100, 0b01100110, 0b01100000, 0b01100000, 0b01100000, 0b01100110, 0b00111100, 0b00000000}, // C
-        {0b01111100, 0b01100110, 0b01100110, 0b01100110, 0b01100110, 0b01100110, 0b01111100, 0b00000000}, // D
-        {0b01111110, 0b01100000, 0b01100000, 0b01111100, 0b01100000, 0b01100000, 0b01111110, 0b00000000}, // E
-        {0b01111110, 0b01100000, 0b01100000, 0b01111100, 0b01100000, 0b01100000, 0b01100000, 0b00000000}  // F
+    // Define a simple 5x9 font for characters 0-9 and A-F
+    uint8_t font[16][9] = {
+        {0b01110, 0b10001, 0b10011, 0b10101, 0b11001, 0b10001, 0b01110, 0b00000, 0b00000}, // 0
+        {0b00100, 0b01100, 0b00100, 0b00100, 0b00100, 0b00100, 0b01110, 0b00000, 0b00000}, // 1
+        {0b01110, 0b10001, 0b00001, 0b00110, 0b01000, 0b10000, 0b11111, 0b00000, 0b00000}, // 2
+        {0b01110, 0b10001, 0b00001, 0b00110, 0b00001, 0b10001, 0b01110, 0b00000, 0b00000}, // 3
+        {0b00010, 0b00110, 0b01010, 0b10010, 0b11111, 0b00010, 0b00010, 0b00000, 0b00000}, // 4
+        {0b11111, 0b10000, 0b11110, 0b00001, 0b00001, 0b10001, 0b01110, 0b00000, 0b00000}, // 5
+        {0b01110, 0b10001, 0b10000, 0b11110, 0b10001, 0b10001, 0b01110, 0b00000, 0b00000}, // 6
+        {0b11111, 0b00001, 0b00010, 0b00100, 0b01000, 0b10000, 0b10000, 0b00000, 0b00000}, // 7
+        {0b01110, 0b10001, 0b10001, 0b01110, 0b10001, 0b10001, 0b01110, 0b00000, 0b00000}, // 8
+        {0b01110, 0b10001, 0b10001, 0b01111, 0b00001, 0b10001, 0b01110, 0b00000, 0b00000}, // 9
+        {0b01110, 0b10001, 0b10001, 0b11111, 0b10001, 0b10001, 0b10001, 0b00000, 0b00000}, // A
+        {0b11110, 0b10001, 0b10001, 0b11110, 0b10001, 0b10001, 0b11110, 0b00000, 0b00000}, // B
+        {0b01110, 0b10001, 0b10000, 0b10000, 0b10000, 0b10001, 0b01110, 0b00000, 0b00000}, // C
+        {0b11110, 0b10001, 0b10001, 0b10001, 0b10001, 0b10001, 0b11110, 0b00000, 0b00000}, // D
+        {0b11111, 0b10000, 0b10000, 0b11110, 0b10000, 0b10000, 0b11111, 0b00000, 0b00000}, // E
+        {0b11111, 0b10000, 0b10000, 0b11110, 0b10000, 0b10000, 0b10000, 0b00000, 0b00000}  // F
     };
 
     // Set the draw color to black (background)
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    //SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 
     // Set the draw color to white (foreground)
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
     // Draw the character to the screen with scaling
-    for (int row = 0; row < 8; ++row) {
-        for (int col = 0; col < 8; ++col) {
-            if (font[value & 0x0F][row] & (1 << (7 - col))) {
+    for (int row = 0; row < 9; ++row) {
+        for (int col = 0; col < 5; ++col) {
+            if (font[value & 0x0F][row] & (1 << (4 - col))) {
                 for (int dx = 0; dx < scale; ++dx) {
                     for (int dy = 0; dy < scale; ++dy) {
                         SDL_RenderDrawPoint(renderer, x + col * scale + dx, y + row * scale + dy);
@@ -84,11 +87,19 @@ void runEmulator() {
         std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
         
     }
-    SDL_Window* window = SDL_CreateWindow("SAA5050 Emulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 960, SDL_WINDOW_SHOWN);
+    int scale = 3;
+     int charWidth = 5 + 1; // 5 pixels wide + 1 pixel space
+    int charHeight = 9 + 1; // 9 pixels high + 1 pixel space
+    int windowWidth = 40 * charWidth * scale;
+    int windowHeight = 24 * charHeight * scale;
+
+    SDL_Window* window = SDL_CreateWindow("SAA5050 Emulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowWidth, windowHeight, SDL_WINDOW_SHOWN);
     if (window == nullptr) {
         std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
+        return;
     }
+
 
     SAA5050 saa5050(window);
 
